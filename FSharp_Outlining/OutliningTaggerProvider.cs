950a460bdd65da1db7822e25bc4131c850c056f8
mje2 <matteskolin@gmail.com>
2018-12-24 11:08:53 -0600

    initial commit

---
+++ FSharp_Outlining/OutliningTaggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace FSharp_Outlining
+{
+    [Export(typeof(ITaggerProvider))]
+    [TagType(typeof(IOutliningRegionTag))]
+    [ContentType("text")]
+    internal sealed class OutliningTaggerProvider:ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            //create a single tagger for each buffer.
+            ITagger<T> Sc() => new OutliningTagger(buffer) as ITagger<T>;
+            return buffer.Properties.GetOrCreateSingletonProperty(Sc);
+        }
+
+    }
+}
